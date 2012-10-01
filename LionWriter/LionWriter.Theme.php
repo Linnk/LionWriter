84df72775b841cb71ce4ab9c500a18d3a934f78a
Juan I. Benavides <linnk87@gmail.com>
2012-10-01 07:43:10 -0500

    Simple helpers for use in the view.

--- LionWriter/LionWriter.Theme.php
+++ LionWriter/LionWriter.Theme.php
@@ -6,14 +6,67 @@ class LionWriterTheme extends LionWriterView
 {
 	public function link($html, $magic_url, $attributes = array())
 	{
+		$attributes_html = $this->htmlFromAttributes($attributes);
+		$url = $this->URLFromMagicURL($magic_url);
+
+		return '<a'.$attributes_html.'>'.$html.'</a>';
 	}
-	public function css($magic_url, $attributes = array())
+	public function css($css_name, $attributes = array())
 	{
+		$url = $this->URLFromMagicURL('/css/'.$css_name.'.css');
+		
+		$attributes = $attributes + array(
+			'rel' => 'stylesheet',
+			'type' => 'text/css',
+			'href' => $url
+		);
+
+		$attributes_html = $this->htmlFromAttributes($attributes);
+
+		return '<link'.$attributes_html.' /> ';
 	}
-	public function javascript($magic_url, $attributes = array())
+	public function javascript($js_name, $attributes = array())
 	{
+		$url = $this->URLFromMagicURL('/js/'.$js_name.'.js');
+		
+		$attributes = $attributes + array(
+			'type' => 'text/javascript',
+			'src' => $url
+		);
+
+		$attributes_html = $this->htmlFromAttributes($attributes);
+
+		return '<script'.$attributes_html.'></script>';
 	}
-	public function image($magic_url, $url, $attributes = array())
+	public function image($image_filename, $attributes = array())
 	{
+		$url = $this->URLFromMagicURL('/img/'.$image_filename);
+		
+		$attributes = $attributes + array(
+			'border' => '0',
+			'src' => $url
+		);
+
+		$attributes_html = $this->htmlFromAttributes($attributes);
+
+		return '<img'.$attributes_html.' />';
+	}
+	protected function htmlFromAttributes($attributes)
+	{
+		$html = '';
+		foreach($attributes as $attribute => $value)
+		{
+			$html .= ' '.$attribute.'="'.$value.'"';
+		}
+		return $html;
+	}
+	protected function URLFromMagicURL($magic_url)
+	{
+		if(LION_REWRITE)
+		{
+			return $magic_url;
+		}
+
+		return '/index.php'.$magic_url;
 	}
 }
