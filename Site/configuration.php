bc53071e15c8aeba64bd40d489f2f84685b87c9d
Juan I. Benavides <linnk87@gmail.com>
2012-10-06 00:37:45 -0500

    Preparing configuration.php file for themes organization.

--- Site/configuration.php
+++ Site/configuration.php
@@ -19,7 +19,24 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 /**
- * First, we are connecting '/' (base path) to all the 'articles' in
+ * First, set up the theme LionWritter will use for your site.
+ */
+	//LionWriter::setTheme('CleanCanvas');
+	/*
+	 *	Lets use the favicon inside the theme. It's good idea use it that way.
+	 *
+	 *	Both /Content & /Webroot/files are content, they should be in the same
+	 *	folder, but the problem is that /Content must be private and the files
+	 *	public. It is good idea keep it separate?
+	 *
+	 *	The configuration.php file must be in /Webroot because is general propose
+	 *	file. But what is it defined inside, have affect in the theme selected. So,
+	 *	what now?
+	 *
+	 *	The bootstrap.php file definitly must be in its folder theme.
+	 */
+/**
+ * Here, we are connecting '/' (base path) to all the 'articles' in
  * the Content, but this will fetch all in sort descending. That's why
  * you must setup a special view to show properly.
  */
@@ -56,3 +73,4 @@
  * If someone make a request to '/contact' path, it will try to find a
  * contact.md file and try render it with the default layout and default view.
  */
+ 
