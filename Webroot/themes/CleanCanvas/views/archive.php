32b1ea0614f3befa3c8ab6ccfb2fa9ff8587ccbf
Juan I. Benavides <linnk87@gmail.com>
2012-11-19 03:23:55 -0600

    A new script.

--- Webroot/themes/CleanCanvas/views/archive.php
+++ Webroot/themes/CleanCanvas/views/archive.php
@@ -1,8 +1,12 @@
-<div class="row row-static">
-	<h2>List of pages:</h2>
-	<ol class="list-pages">
+<?php echo $this->element('header') ?>
+
+<hr />
+
+<h2>All the content in this universe:</h2>
+<ol class="list-pages">
 	<?php foreach($pages as $page): ?>
-		<li><span class="page-date"><?php echo $page['date'] ?></span> <span class="script-dialog">—</span> <a href="<?php echo $page['permalink'] ?>"><?php echo $page['title'] ?></a></li>
+		<li><span class="page-date muted"><?php echo $page['date'] ?></span> <span class="script-dialog">—</span> <a href="<?php echo $page['permalink'] ?>"><?php echo $page['title'] ?></a></li>
 	<?php endforeach; ?>
-	</ol>
-</div>
+</ol>
+
+<?php echo $this->element('footer') ?>
