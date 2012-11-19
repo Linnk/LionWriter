a161a81ed60da66e5b23c45884584c843a96e991
Juan I. Benavides <linnk87@gmail.com>
2012-11-19 02:44:10 -0600

    Some changes in the default view and layout.

--- Webroot/themes/CleanCanvas/layouts/default.php
+++ Webroot/themes/CleanCanvas/layouts/default.php
@@ -2,40 +2,20 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title><?php echo isset($title_for_layout) ? $title_for_layout : 'LionWriter' ?></title>
+	<title><?php echo isset($title_for_layout) ? $title_for_layout : 'The Lion Writer' ?></title>
 	<meta name="description" content="<?php echo isset($description_for_layout) ? $description_for_layout : 'The content management system for hipster hackers.' ?>" />
 	<meta name="keywords" content="<?php echo isset($keywords_for_layout) ? $keywords_for_layout : 'hipster, hacker, content management system' ?>" />
 	<link href="<?php echo LION_THEME ?>/img/favicon.ico" type="image/x-icon" rel="icon" />
-	<?php echo $this->css('reset'); ?>
-	<?php // <link href='http://fonts.googleapis.com/css?family=Overlock:400,700,400italic,700italic' rel='stylesheet' type='text/css'> ?>
-	<?php // <link href='http://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,400italic,700,700italic' rel='stylesheet' type='text/css'> ?>
-	<?php echo $this->css('lion'); ?>
-	<?php echo $this->javascript('mootools-core-1.4.5-full-nocompat-yc'); ?>
-	<?php echo $this->javascript('writer'); ?>
+	<?php echo $this->css('bootstrap'); ?>
+	<?php echo $this->css('cleancanvas'); ?>
 </head>
 </html>
 <body id="LionWriter" class="real">
-	<div id="header">
-		<div class="container">
-			<h1><a href="/">LionWriter</a></h1>
-			<div class="sections">
-				<?php echo $this->link('Archive', '/archive') ?>
-				<?php echo $this->link('About', '/about') ?>
-			</div>
-			
-			<div class="clear">
-		</div>
-	</div>
-	<div id="wrapper">
-		<div class="container">
-	
+
+	<div class="container">
 <?php echo $content_for_layout ?>
-	
-		</div>
 	</div>
-	<div id="footer">
-		<?php echo $this->element('footer') ?>
-	</div>
+
 </body>
 </body>
 </html>
--- Webroot/themes/CleanCanvas/views/default.php
+++ Webroot/themes/CleanCanvas/views/default.php
@@ -1,25 +1,8 @@
-<div class="content">
-
-<?php if(isset($page)): ?>
-
+<div class="single-page">
 	<h2><?php echo $page['title'] ?></h2>
 	<?php echo $page['content'] ?>
-
-<?php elseif(isset($pages)): ?>
-
-	<hr />
 	
-	<h2>List of pages:</h2>
-	<ol class="list-pages">
-	<?php foreach($pages as $page): ?>
-		<li><span class="page-date"><?php echo $page['date'] ?></span> <span class="script-dialog">—</span> <a href="<?php echo $page['permalink'] ?>"><?php echo $page['title'] ?></a></li>
-	<?php endforeach; ?>
-	</ol>
+	<hr />
 
-<?php else: ?>
-
-	<p>I didn't recieve anything :( </p>
-
-<?php endif; ?>
-
+	<?php echo $this->element('page-author') ?>
 </div>
