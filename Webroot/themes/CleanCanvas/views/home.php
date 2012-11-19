cdbcb2624931fdb8bf87c1a42583879991f27dbe
Juan I. Benavides <linnk87@gmail.com>
2012-11-19 02:43:51 -0600

    I decided only to have header and footer in this view.

--- Webroot/themes/CleanCanvas/views/home.php
+++ Webroot/themes/CleanCanvas/views/home.php
@@ -1,20 +1,36 @@
-<div class="content">
+<div class="row row-static" id="header">
+	<h1 class="span10"><a href="/">Lion Writer</a></h1>
+	<div class="sections span1">
+		<?php echo $this->link('Archive', '/archive') ?>
+	</div>
+	<div class="sections span1">
+		<?php echo $this->link('About', '/about') ?>
+	</div>
+	<hr />
+</div>
 
 <?php if(isset($pages[0])): $page = array_shift($pages);?>
-
+<div class="row row-static">
+	<div class="span8">
 	<h2><a href="<?php echo $page['permalink'] ?>"><?php echo $page['title'] ?></a></h2>
 	<?php echo $page['content'] ?>
-
+	</div>
+</div>
 <?php endif; ?>
 
-	<h2>More</h2>
-	<ul class="list-pages">
+<div class="row row-static">
 	<?php foreach($pages as $page): ?>
-		<li>
-			<span class="page-date"><?php echo $page['date'] ?></span> <span class="script-dialog">—</span> <a href="<?php echo $page['permalink'] ?>"><?php echo $page['title'] ?></a>
-			<p><?php echo $page['excerpt'] ?>…</p>
-		</li>
+	<div class="span4">
+		<h3><a href="<?php echo $page['permalink'] ?>"><?php echo $page['title'] ?></a></h3>
+		<p><?php echo $page['excerpt'] ?>…</p>
+		<p class="page-date"><?php echo $page['date'] ?></p>
+	</div>
 	<?php endforeach; ?>
-	</ul>
-	
+</div>
+
+
+<hr />
+
+<div id="footer" class="container">
+	<?php echo $this->element('footer') ?>
 </div>
