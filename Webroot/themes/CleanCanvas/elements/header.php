abb1d8ef629df56ed7500e372448c6359dcb4d29
Juan I. Benavides <linnk87@gmail.com>
2012-11-19 03:23:44 -0600

    Header in its own element.

---
+++ Webroot/themes/CleanCanvas/elements/header.php
@@ -0,0 +1,10 @@
+<div class="row row-static" id="header">
+	<h1 class="span10"><a href="/">Lion Writer</a></h1>
+	<div class="sections span1">
+		<?php echo $this->link('Archive', '/archive') ?>
+	</div>
+	<div class="sections span1">
+		<?php echo $this->link('About', '/about') ?>
+	</div>
+	<hr />
+</div>
