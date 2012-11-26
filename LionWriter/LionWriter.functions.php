a1f9665b350b7de5df40588fca5184c32ebe8ae1
Juan I. Benavides <linnk87@gmail.com>
2012-11-26 01:59:08 -0600

    Code documentation for this file.

--- LionWriter/LionWriter.functions.php
+++ LionWriter/LionWriter.functions.php
@@ -1,6 +1,31 @@
 <?php
-
-
+/**
+ * Useful functions
+ *
+ * In this files are declared some useful functions used on debugging and
+ * also inside de core of LionWriter itself.
+ *
+ * PHP 5
+ *
+ * LionWriter : Rapid Development Framework (http://lionwriter.com)
+ * Copyright 2012, Juan I. Benavides. (http://juanbenavides.info)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2012, Juan I. Benavides. (http://juanbenavides.info)
+ * @link          http://lionwriter.com LionWriter Project
+ * @package       Views
+ * @since         LionWriter v 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+/**
+ * Check if the pr functions exists. If not,  declare it. This echoes
+ * a $mixed content with print_r inside a <pre> element.
+ *
+ * @param $mixed
+ * @return void
+ */
 if(!function_exists('pr'))
 {
 	function pr($mixed)
@@ -8,6 +33,13 @@ if(!function_exists('pr'))
 		echo '<pre>'.print_r($mixed, true).'</pre>';
 	}
 }
+/**
+ * Check if the vd functions exists. If not, declare it. This echoes
+ * a $mixed content with var_dump inside a <pre> element.
+ *
+ * @param $mixed
+ * @return void
+ */
 if(!function_exists('vd'))
 {
 	function vd($mixed)
@@ -17,10 +49,18 @@ if(!function_exists('vd'))
 		echo '</pre>';
 	}
 }
+/**
+ * Makes a summary from a given content string in lenght defined.
+ *
+ * @param STRING $content
+ * @param INTEGER $lenght The maximum size of the summary. Could be shorter.
+ * @param BOOLEAN $p Determine if the summary will use <p> elements for breaklines
+ * @return void
+ */
 function substract_summary($content, $lenght, $p = false)
 {
 	if(strlen($content) <= $lenght)
-		return content;
+		return $content;
 	
 	$content = substr($content, 0, $lenght);
 	
