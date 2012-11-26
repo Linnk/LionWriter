fd6b30ee27d761d4073161b05979eddbd78fd44e
Juan I. Benavides <linnk87@gmail.com>
2012-11-26 00:07:48 -0600

    Added the nice page image to the single pages. :)

--- Webroot/themes/CleanCanvas/views/page.php
+++ Webroot/themes/CleanCanvas/views/page.php
@@ -1,4 +1,9 @@
-<div class="single-page">
+<div class="single-page <?php echo isset($page['image']) ? 'with-image' : '' ?>">
+	<?php if(isset($page['image'])): ?>
+	<div class="page-image">
+		<img src="/files/<?php echo $page['image'] ?>" class="img " width="100%"/>
+	</div>
+	<?php endif; ?>
 	<h1><?php echo $page['title'] ?></h1>
 	<?php echo $page['content'] ?>
 	
