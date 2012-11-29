7328e7f5496545f03b4a81d1197c0196633b353c
Juan I. Benavides <linnk87@gmail.com>
2012-11-29 15:28:13 -0600

    Improvements to the single page mobile version.

--- Webroot/themes/CleanCanvas/views/page.php
+++ Webroot/themes/CleanCanvas/views/page.php
@@ -1,9 +1,10 @@
+<?php if(isset($page['image'])): ?>
+<div class="page-image">
+	<img src="/files/<?php echo $page['image'] ?>" class="img " width="100%"/>
+</div>
+<?php endif; ?>
+
 <div class="single-page <?php echo isset($page['image']) ? 'with-image' : '' ?>">
-	<?php if(isset($page['image'])): ?>
-	<div class="page-image">
-		<img src="/files/<?php echo $page['image'] ?>" class="img " width="100%"/>
-	</div>
-	<?php endif; ?>
 	<h1><?php echo $page['title'] ?></h1>
 	<?php echo $page['content'] ?>
 	
