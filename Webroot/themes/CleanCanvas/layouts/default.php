38761e5cee9534d3dd1596197bfd1d7ef3faef65
Juan I. Benavides <linnk87@gmail.com>
2012-11-29 15:16:38 -0600

    Missing viewport meta property to display real mobile version on mobile browsers.

--- Webroot/themes/CleanCanvas/layouts/default.php
+++ Webroot/themes/CleanCanvas/layouts/default.php
@@ -5,6 +5,7 @@
 	<title><?php echo isset($title_for_layout) ? $title_for_layout : 'The Lion Writer' ?></title>
 	<meta name="description" content="<?php echo isset($description_for_layout) ? $description_for_layout : 'The content management system for hipster hackers.' ?>" />
 	<meta name="keywords" content="<?php echo isset($keywords_for_layout) ? $keywords_for_layout : 'hipster, hacker, content management system' ?>" />
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 	<link href="<?php echo LION_THEME ?>/img/favicon.ico" type="image/x-icon" rel="icon" />
 	<?php echo $this->css('bootstrap'); ?>
 	<?php echo $this->css('cleancanvas'); ?>
